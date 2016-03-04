fcfdcc40bc919c2acc7c8245921e37da1b04f705
chuffey.1240 <chuffey.1240@yandex.ru>
2016-03-04 10:00:46 +0300

    1240
    update

--- app/src/main/java/ru/l240/miband/MainActivity.java
+++ app/src/main/java/ru/l240/miband/MainActivity.java
@@ -3,18 +3,24 @@ package ru.l240.miband;
 import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import ru.l240.miband.utils.PrefUtils;
+
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-public class MainActivity extends AppCompatActivity implements BluetoothAdapter.LeScanCallback {
+public class MainActivity extends AppCompatActivity implements BluetoothAdapter.LeScanCallback, SwipeRefreshLayout.OnRefreshListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     private ProgressBar pb;
     private TextView tv;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,10 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
         tv = (TextView) findViewById(R.id.tvMainActivitySearch);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.srl);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        // делаем повеселее
+        mSwipeRefreshLayout.setColorScheme(R.color.s1, R.color.s2, R.color.s3, R.color.s4);
     }
 
     @Override
@@ -29,6 +39,13 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
         if (device != null && device.getName().contains("MI1S")) {
             System.out.println(device.getAddress());
             tv.setText("Браслет найден, синхронизируюсь...");
+            PrefUtils.saveAddress(MainActivity.this, device.getAddress());
+
         }
+    }
+
+    @Override
+    public void onRefresh() {
+        Log.d(TAG, "refreshing");
     }
 }
